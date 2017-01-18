fe49cf015a3645a1935fd4a31589ac8acd6b8d90
Bradley Schmerl <schmerl@cs.cmu.edu>
2017-01-18 09:40:12 -0500

    Fixed permissions

--- installation/setup-cp1.sh
+++ installation/setup-cp1.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+# Fix up potential executable permissions on python files
 source /opt/ros/indigo/setup.bash
 cd catkin_ws/src
+find . -name "*.py" -exec chmod +x {} \;
 catkin_init_workspace
 cd ..
 catkin_make
