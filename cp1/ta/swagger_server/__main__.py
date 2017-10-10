c5cd1e647398734b5211d9254f9c9e03ae768c56
Ian Voysey <iev@cs.cmu.edu>
2017-10-10 16:58:28 -0400

    #24 for CP1, modulo a fussy python issue

--- cp1/ta/swagger_server/__main__.py
+++ cp1/ta/swagger_server/__main__.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 import connexion
 from .encoder import JSONEncoder
 import logging
 
 if __name__ == '__main__':
+    try:
+        print("working with brass-th: %s" % sys.argv[1])
+    except IndexError:
+        print("please provide exactly one argument, an address to the TH")
+        sys.exit(1)
+
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'CP1'}, strict_validation=True)
