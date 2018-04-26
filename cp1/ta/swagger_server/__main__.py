83ce2f605c6c95cee2a6138cd4e0c90896583d28
Ian Voysey <iev@cs.cmu.edu>
2018-04-26 16:40:15 -0400

    TA updates

--- cp1/ta/swagger_server/__main__.py
+++ cp1/ta/swagger_server/__main__.py
@@ -20,6 +20,8 @@ import requests
 import time
 import random
 
+from rainbow_interface import RainbowInterface
+
 from swagger_client.rest import ApiException
 from swagger_client import DefaultApi
 from swagger_client.models.parameters import Parameters
@@ -29,7 +31,6 @@ from swagger_client.models.parameters_2 import Parameters2
 import swagger_server.config as config
 
 if __name__ == '__main__':
-
     # Parameter parsing, to set up TH
     if len(sys.argv) != 2:
       print ("No URI TH passed in!")
@@ -39,9 +40,10 @@ if __name__ == '__main__':
 
     # Set up TA server and logging
     app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = JSONEncoder
+    app.app.json_encoder = JSONEncoder ## possibly busted; unclear
     app.add_api('swagger.yaml', arguments={'title': 'CP1'}, strict_validation=True)
 
+    # capture the logger
     logger = logging.getLogger('werkzeug')
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler('access.log')
@@ -53,24 +55,94 @@ if __name__ == '__main__':
 
     app.app.before_request(log_request_info)
 
-    # Connect to th
+    # build the TH API object
     thApi = DefaultApi()
     thApi.api_client.host = th_uri
+    config.thApi = thApi
 
-    # Hack: Try sending stuff to TH
+    ## todo: utils module some how? copied from CP3
+    def fail_hard(s):
+        logger.debug(s)
+        if th_connected:
+            thApi.error_post(Parameters(s)) ## todo: possibly the wrong class here
+        raise Exception(s)
+
+    ## start the sequence diagram: post to ready to get configuration data
     try:
-      logger.debug("Sending test parsing-error to th")
-      thApi.error_post(parameters=Parameters("parsing-error", "This is a test error post to th"))
+        logger.debug("posting to /ready")
+        ready_resp = thApi.ready_post()
+        logger.debug("recieved response from /ready: %s" % ready_resp)
     except Exception as e:
-      logger.debug("Failed to connect with th")
-      logger.debug(traceback.format_exc())
+        ## this isn't a call to fail_hard because the TH isn't
+        ## responding at all; we have to hope that LL notices the log
+        ## output and that this happens only very rarely if at all
+        logger.debug("failed to connect with th")
+        raise e
 
+    ## dynamic checks on ready response
+    if ready_resp.target_locs == []:
+        fail_hard("malformed response from ready: target_locs must not be the empty list")
 
+    if ready_resp.start_loc == ready_resp.target_locs[0]:
+        fail_hard("malformed response from ready: start-loc must not be the same as the first item of target-locs")
+
+    ## todo: probably in a util file
+    def check_adj(l):
+        for x , y in zip(l, l[1:]):
+            if x == y:
+                return False
+        return True
+
+    if not check_adj(ready_resp.target_locs):
+        fail_hard("malformed response from ready: target-locs contains adjacent equal elements")
+
+    ## once the response is checked, write it to ~/ready
+    logger.debug("writing checked /ready message to ~/ready")
+    fo = open(os.path.expanduser('~/ready'), 'w')
+    fo.write('%s' %ready_resp) #todo: this may or may not be JSON; check once we can run it
+    fo.close()
 
     # Init me as a node
+    logger.debug("initializing cp1_ta ros node")
     rospy.init_node("cp1_ta")
 
-    fake_semantics(thApi,5000)
+    ## todo: instead of sleeping, listen to a topic for whatever indicated "odom recieved"
+    logger.debug("waiting for move_base (emulates watching for odom_recieved)")
+    move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    ## todo: Ian - should this wait for some period (e.g., timeout=60s), otherwise we will wait forever
+    move_base_started = move_base.wait_for_server()
+    if not move_base_started:
+        fail_hard("fatal error: navigation stack has failed to start")
+
+    ## TODO: start gazebo interface??
+
+    if ready_resp.level == "c":
+        try:
+            rainbow_log = open(os.path.expanduser("~/rainbow.log"),'w')
+            rainbow = RainbowInterface()
+            rainbow.launchRainbow("cp1", rainbow_log)
+            ok = rainbow.startRainbow()
+            if not ok:
+                fail_hard("did not connect to rainbow in a timely fashion")
+        except Exception as e:
+            fail_hard("failed to connecto to rainbow: %s " %e)
+
+    ## subscribe to rostopics
+    def energy_cb(msg):
+        """call back to update the global battery state from the ros topic"""
+        ## todo: this may be the wrong format (int vs float)
+        config.battery = msg.data
+
+    sub_voltage = rospy.Subscriber("/energy_monitor/energy_level", Float64, energy_cb)
+
+    if not ready_resp.level == "c":
+        logger.debug("sending live status message")
+        ## todo: i have no idea what rospy is going to say the sim
+        ## time is. probably 0.
+        live_resp = ??? ## thApi.status_post(Parameters1("live","CP3 TA ready to recieve inital perturbs and start in non-adaptive case",rospy.Time.now().secs,[],[],[])) ## todo placeholder value
+        config.logger.debug("repsonse from TH to live: %s" % response)
+
+    logger.debug("starting TA REST interface")
 
     # Start the TA listening
     app.run(host='0.0.0.0',port=5000)
