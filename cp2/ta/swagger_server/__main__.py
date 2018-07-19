35f9b8f85427941449ecbb3f72bf5485ce8d4189
ChrisTimperley <christimperley@googlemail.com>
2018-07-19 17:14:10 -0400

    disable kaskara logging for CP2

--- cp2/ta/swagger_server/__main__.py
+++ cp2/ta/swagger_server/__main__.py
@@ -182,7 +182,7 @@ def main():
     setup_logger('darjeeling', stdout=True)
     setup_logger('boggart', level=logging.ERROR)
     setup_logger('bugzoo', level=logging.ERROR)
-    setup_logger('kaskara', level=logging.DEBUG)
+    # setup_logger('kaskara', level=logging.DEBUG)
     # setup_logger('rooibos', level=logging.WARNING)
 
     def log_request_info():
